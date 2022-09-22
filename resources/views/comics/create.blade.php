df219e4d88463bcd25516d7c1a59e87a36e8a510
virtu-alex <alessio.cordari@gmail.com>
2022-09-22 18:11:11 +0200

    feat: Styled form

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -2,36 +2,57 @@
 
 
 @section('main-content')
-<div class="container bg-create bg-dark">
-    <form action="{{route('comics.store')}}" method="POST">
-        @csrf
-        <div class="row my-4 g-2 p-5">
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Title" name="title">
+    <div class="container bg-create bg-dark">
+        <form action="{{ route('comics.store') }}" method="POST">
+            @csrf
+            <div class="row my-4 g-2 p-5">
+                <div class="col">
+                    <div class="col">
+                        Title
+                    </div>
+                    <input type="text" class="form-control" placeholder="Title" name="title">
+                </div>
+                <div class="col">
+                    <div class="col">
+                        Type
+                    </div>
+                    <input type="text" class="form-control" placeholder="Type" name="type">
+                </div>
+                <div class="col">
+                    <div class="col">
+                        Thumb
+                    </div>
+                    <input type="text" class="form-control" placeholder="Thumb" name="thumb">
+                </div>
+                <div class="col">
+                    <div class="col">
+                        Price
+                    </div>
+                    <input type="text" class="form-control" placeholder="Price" name="price">
+                </div>
+                <div class="col">
+                    <div class="col">
+                        Series
+                    </div>
+                    <input type="text" class="form-control" placeholder="Series" name="series">
+                </div>
+                <div class="col">
+                    <div class="col">
+                        Sale date
+                    </div>
+                    <input type="text" class="form-control" placeholder="Sale Date" name="sale_date">
+                </div>
+                <div class="col-12">
+                    <div class="col-12">
+                        Description
+                    </div>
+                    <input type="text" class="form-control" placeholder="Description" name="description">
+                </div>
+
+                <div class="col-12 text-center pt-3">
+                    <button type="submit" class="btn btn-warning border">Submit</button>
+                </div>
             </div>
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Type" name="type">
-            </div>
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Thumb" name="thumb">
-            </div>
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Price" name="price">
-            </div>
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Series" name="series">
-            </div>
-            <div class="col">
-                <input type="text" class="form-control" placeholder="Sale Date" name="sale_date">
-            </div>
-            <div class="col-12">
-                <input type="text" class="form-control" placeholder="Description" name="description">
-            </div>
-            
-            <div class="col-12 text-center pt-3">
-                <button type="submit" class="btn btn-warning border">Submit</button>
-            </div>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
 @endsection
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -7,29 +7,50 @@
             @method('PUT')
             <div class="row my-3 bg-create g-2 bg-dark p-5">
                 <div class="col">
+                    <div class="col">
+                        Title
+                    </div>
                     <input type="text" class="form-control" placeholder="Title" name="title" value="{{ $comic->title }}">
                 </div>
                 <div class="col">
+                    <div class="col">
+                        Type
+                    </div>
                     <input type="text" class="form-control" placeholder="Type" name="type"
                         value="{{ $comic->type }}">
                 </div>
                 <div class="col">
+                    <div class="col">
+                        Thumb
+                    </div>
                     <input type="text" class="form-control" placeholder="Thumb" name="thumb"
                         value="{{ $comic->thumb }}">
                 </div>
                 <div class="col">
+                    <div class="col">
+                        Price
+                    </div>
                     <input type="text" class="form-control" placeholder="Price" name="price"
                         value="{{ $comic->price }}">
                 </div>
                 <div class="col">
+                    <div class="col">
+                        Series
+                    </div>
                     <input type="text" class="form-control" placeholder="Series" name="series"
                         value="{{ $comic->series }}">
                 </div>
                 <div class="col">
+                    <div class="col">
+                        Sale date
+                    </div>
                     <input type="text" class="form-control" placeholder="Sale Date" name="sale_date"
                         value="{{ $comic->sale_date }}">
                 </div>
                 <div class="col-12">
+                    <div class="col-12">
+                        Description
+                    </div>
                     <input type="text" class="form-control" placeholder="Description" name="description"
                         value="{{ $comic->description }}">
                 </div>
