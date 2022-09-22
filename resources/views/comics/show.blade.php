45dad4913a9442a49d9138726cba4e3276335e0b
virtu-alex <alessio.cordari@gmail.com>
2022-09-22 14:21:09 +0200

    feat: Show buttons ok

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -1,27 +1,41 @@
 @extends('layouts.main')
 
 @section('main-content')
+    <div class="main-card">
+        <img src="{{ $comic->thumb }}" alt="">
 
-        <div class="main-card">
-            <img src="{{ $comic->thumb }}" alt="">
-
-            <div>
-                {{ $comic->price }}
-            </div>
-
-            <div>
-                {{ $comic->title }}
-            </div>
-
-            <div>
-                {{ $comic->sale_date }}
-            </div>
-
-            <div>
-                {{ $comic->type }}
-            </div>
-            <div>
-                {{ $comic->description }}
-            </div>
+        <div>
+            {{ $comic->price }}
         </div>
+
+        <div>
+            {{ $comic->title }}
+        </div>
+
+        <div>
+            {{ $comic->sale_date }}
+        </div>
+
+        <div>
+            {{ $comic->type }}
+        </div>
+        <div>
+            {{ $comic->description }}
+        </div>
+    </div>
+    <div class="row my-3">
+        <div class="col-2">
+                <a class="text-white btn btn-secondary" href="{{ route('comics.index') }}">Torna indietro</a>
+        </div>
+        <div class="col-2">
+                <a class="text-white btn btn-warning" href="{{ route('comics.edit', $comic->id) }}">Modifica</a>
+        </div>
+        <div class="col-2">
+            <form method="POST" action="{{ route('comics.destroy', $comic->id) }}">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-danger" type="submit">Cancella</button>
+            </form>
+        </div>
+    </div>
 @endsection
